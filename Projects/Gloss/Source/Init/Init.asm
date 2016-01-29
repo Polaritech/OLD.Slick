a62a8f85b87e3b70ef8fb63a5da205c9991c3c99
Adrian J. Collado <acollado@polaritech.com>
2016-01-28 21:37:11 -0500

    Added some A20 line stuff for the IAPX-86 bootloader.

---
+++ Projects/Gloss/Source/Init/Init.asm
@@ -0,0 +1,10 @@
+;=============================================================================;
+; GLOSS - Generic Loader for Operating SystemS                                ;
+; An extensible and configurable bootloader.                                  ;
+;-----------------------------------------------------------------------------;
+; Copyright (C) 2013-2016 ~ Adrian J. Collado       [acollado@polaritech.com] ;
+; Copyright (C) 2015-2016 ~ Polaritech, Inc.         [https://polaritech.com] ;
+; All Rights Reserved                                                         ;
+;=============================================================================;
+
+%include "AssemblyFunctions/IAPX-86/A20/A20.inc"
